6fffd39f4f75b5bf0939c554dd4750eb81bcafec
Parham Alvani <parham.alvani@gmail.com>
2020-06-04 20:18:28 +0430

    feat: Improve Reverse String

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,7 +1,3 @@
-use std::iter::FromIterator;
-
 pub fn reverse(input: &str) -> String {
-    let input = String::from(input);
-
-    String::from_iter(input.chars().rev())
+    input.chars().rev().collect()
 }
