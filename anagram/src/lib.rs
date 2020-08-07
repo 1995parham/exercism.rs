4723f206aff5527fbd7c40433a7e4f58f092ccc1
Parham Alvani <parham.alvani@gmail.com>
2020-08-07 08:10:51 +0430

    fix: Correct Lint Issues

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -12,25 +12,19 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'
         }
     }
 
-    return s;
+    s
 }
 
 fn is_anagram(word: &str, possible_anagram: &str) -> bool {
     let mut characters: HashMap<char, i32> = HashMap::new();
 
     for b in word.to_lowercase().chars() {
-        characters.insert(b, *characters.get(&b).unwrap_or(&0) + 1);
+        *characters.entry(b).or_insert(0) += 1;
     }
 
     for b in possible_anagram.to_lowercase().chars() {
-        characters.insert(b, *characters.get(&b).unwrap_or(&0) - 1);
+        *characters.entry(b).or_insert(0) -= 1;
     }
 
-    for &v in characters.values() {
-        if v != 0 {
-            return false;
-        }
-    }
-
-    return true;
+    characters.values().all(|&v| v == 0)
 }
